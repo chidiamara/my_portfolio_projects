2fab8ecee7a4f694561ffa13313661cbbbde573f
Chidi <chidiamaraekejiuba@gmail.com>
2023-03-06 12:32:52 +0000

    Requesting for new user reg data

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -16,7 +16,20 @@ export const register = async (req, res) => {
             occupation
         } = req.body;
         const salt = await bcrypt.genSalt(); // PASSWORD ENCRYPTION
-        const passwordHash = await bcrypt.hash(password, salt)
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        const newUser = new User ({
+            firstName,
+            lastName,
+            email,
+            password: passwordHash,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 10000),  // USING RANDOM NUMBERS
+            impressions: Math.floor(Math.random() * 10000),  // USING RANDOM NUMBERS
+        })
     } catch (err) {
     }
 }
